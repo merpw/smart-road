af2b592a94170fbf0ed5fa123dc0b2b259b505c2
Maksim Mikhailov <max@mer.pw>
2023-09-08 17:26:49 +0300

    Initialize macroquad
    Add `assets` dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Texture")]
+async fn main() {
+    let texture: Texture2D = load_texture("assets/cat.png").await.unwrap();
+
+    loop {
+        clear_background(LIGHTGRAY);
+        draw_texture(&texture, 0., 0., WHITE);
+        next_frame().await
+    }
 }
