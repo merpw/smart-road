ff93d97885169129e24922380c81a93f523f059b
Maksim Mikhailov <max@mer.pw>
2023-09-09 15:08:10 +0300

    Improve draw_roads

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,15 @@
+use macroquad::color::Color;
 use macroquad::window::Conf;
 
 pub const WINDOW_WIDTH: i32 = 1000;
 pub const WINDOW_HEIGHT: i32 = 1000;
+
+pub const ROAD_WIDTH: f32 = 200.0;
+
+pub const ROAD_LINE_WIDTH: f32 = 5.0;
+
+pub const ROAD_COLOR: Color = Color::new(0.267, 0.294, 0.325, 1.0);
+pub const ROAD_LINE_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.0);
 
 pub fn window_conf() -> Conf {
     Conf {
--- src/draw/roads.rs
+++ src/draw/roads.rs
@@ -1,12 +1,53 @@
+use crate::config::{
+    ROAD_COLOR, ROAD_LINE_COLOR, ROAD_LINE_WIDTH, ROAD_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH,
+};
 use macroquad::prelude::*;
-pub fn draw_roads() {
-    // Vertical lines
-    draw_line(450.0, 0.0, 450.0, 1000.0, 1.0, WHITE);
-    draw_line(500.0, 0.0, 500.0, 1000.0, 1.0, WHITE);
-    draw_line(550.0, 0.0, 550.0, 1000.0, 1.0, WHITE);
 
-    // Horizontal lines
-    draw_line(0.0, 450.0, 1000.0, 450.0, 1.0, WHITE);
-    draw_line(0.0, 500.0, 1000.0, 500.0, 1.0, WHITE);
-    draw_line(0.0, 550.0, 1000.0, 550.0, 1.0, WHITE);
+pub fn draw_roads() {
+    // Vertical road
+    draw_rectangle(
+        (WINDOW_WIDTH as f32 - ROAD_WIDTH) / 2.0,
+        0.0,
+        ROAD_WIDTH,
+        WINDOW_WIDTH as f32,
+        ROAD_COLOR,
+    );
+
+    // Horizontal road
+    draw_rectangle(
+        0.0,
+        (WINDOW_WIDTH as f32 - ROAD_WIDTH) / 2.0,
+        WINDOW_WIDTH as f32,
+        ROAD_WIDTH,
+        ROAD_COLOR,
+    );
+
+    for i in 0..(WINDOW_WIDTH / 100) {
+        // vertical road line
+        draw_rectangle(
+            (WINDOW_WIDTH as f32 - ROAD_LINE_WIDTH) / 2.0,
+            25.0 + i as f32 * 100.0,
+            ROAD_LINE_WIDTH,
+            50.0,
+            ROAD_LINE_COLOR,
+        );
+
+        // horizontal road line
+        draw_rectangle(
+            25.0 + i as f32 * 100.0,
+            (WINDOW_WIDTH as f32 - ROAD_LINE_WIDTH) / 2.0,
+            50.0,
+            ROAD_LINE_WIDTH,
+            ROAD_LINE_COLOR,
+        );
+    }
+
+    // remove lines in the center
+    draw_rectangle(
+        (WINDOW_WIDTH as f32 - ROAD_WIDTH) / 2.0,
+        (WINDOW_HEIGHT as f32 - ROAD_WIDTH) / 2.0,
+        ROAD_WIDTH,
+        ROAD_WIDTH,
+        ROAD_COLOR,
+    );
 }
