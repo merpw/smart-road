0ce80e3ec305915c4ef197a8790c3f2a935fee2b
Maksim Mikhailov <max@mer.pw>
2023-09-09 23:40:42 +0300

    Add center points for paths
    Improve documentation

--- src/traffic/path.rs
+++ src/traffic/path.rs
@@ -16,64 +16,75 @@ pub struct Path {
 //      _______________|     |     |_______________
 //      ← ←                                     ← ←
 // East ---------------             --------------- West
-//      → →                                     → →
-//      _______________             _______________
-//                     |     |     |
-//                     |     |     |
-//                     |     |     |
-//                     |     |     |
-//                     |     |     |
-//                     |  ↓  |  ↑  |
-//                     |  ↓  |  ↑  |
+//      A →            B                        → →
+//      _______________    C        _______________
+//                     |   | |     |
+//                     |   | |     |
+//                     |   | |     |
+//                     |   | |     |
+//                     |   | |     |
+//                     |   ↓ |  ↑  |
+//                     |   D |  ↑  |
 //                         South
+// Path(East, Right) =  [A -> B -> C -> D]
+// A - border_point(East, true)
+// D - border_point(South, false)
+// B - straight_point(East, A, STRAIGHT_LENGTH)
+// C - straight_point(South, D, STRAIGHT_LENGTH)
+
+const STRAIGHT_LENGTH: f32 = (WINDOW_SIZE as f32 - ROAD_WIDTH) / 2.0;
+
+/// Returns the point on the border where the car should appear or disappear
+fn border_point(coming_from: Direction, right_side: bool) -> Vec2 {
+    let car_padding = if right_side {
+        CAR_PADDING
+    } else {
+        -CAR_PADDING
+    };
+    match coming_from {
+        Direction::North => Vec2::new(WINDOW_SIZE as f32 / 2.0 - car_padding, 0.0),
+        Direction::East => Vec2::new(WINDOW_SIZE as f32, WINDOW_SIZE as f32 / 2.0 - car_padding),
+        Direction::South => Vec2::new(WINDOW_SIZE as f32 / 2.0 + car_padding, WINDOW_SIZE as f32),
+        Direction::West => Vec2::new(0.0, WINDOW_SIZE as f32 / 2.0 + car_padding),
+    }
+}
+
+/// Returns the point in center associated with the border point
+fn straight_point(direction: Direction, border_point: Vec2, straight_length: f32) -> Vec2 {
+    match direction {
+        Direction::North => Vec2::new(border_point.x, border_point.y + straight_length),
+        Direction::East => Vec2::new(border_point.x - straight_length, border_point.y),
+        Direction::South => Vec2::new(border_point.x, border_point.y - straight_length),
+        Direction::West => Vec2::new(border_point.x + straight_length, border_point.y),
+    }
+}
 
 impl Path {
-    fn start_point(coming_from: Direction) -> Vec2 {
-        match coming_from {
-            Direction::North => Vec2::new(
-                WINDOW_SIZE as f32 / 2.0 - ROAD_WIDTH / 2.0 + CAR_PADDING,
-                0.0,
-            ),
-            Direction::East => {
-                Vec2::new(WINDOW_SIZE as f32, WINDOW_SIZE as f32 / 2.0 - CAR_PADDING)
-            }
-            Direction::South => {
-                Vec2::new(WINDOW_SIZE as f32 / 2.0 + CAR_PADDING, WINDOW_SIZE as f32)
-            }
-            Direction::West => Vec2::new(0.0, WINDOW_SIZE as f32 / 2.0 + CAR_PADDING),
-        }
-    }
-
-    fn end_point(coming_from: Direction, going_to: Going) -> Vec2 {
+    pub fn new(coming_from: Direction, going_to: Going) -> Self {
         let destination = coming_from.destination(going_to);
 
-        match destination {
-            Direction::North => Vec2::new(WINDOW_SIZE as f32 / 2.0 + CAR_PADDING, 0.0),
-            Direction::East => {
-                Vec2::new(WINDOW_SIZE as f32, WINDOW_SIZE as f32 / 2.0 + CAR_PADDING)
-            }
-            Direction::South => Vec2::new(
-                WINDOW_SIZE as f32 / 2.0 - ROAD_WIDTH / 2.0 + CAR_PADDING,
-                WINDOW_SIZE as f32,
-            ),
-            Direction::West => Vec2::new(0.0, WINDOW_SIZE as f32 / 2.0 - CAR_PADDING),
-        }
-    }
-
-    pub fn new(coming_from: Direction, going_to: Going) -> Self {
-        let start_point = Self::start_point(coming_from);
-        let end_point = Self::end_point(coming_from, going_to);
+        let start_point = border_point(coming_from, true);
+        let end_point = border_point(destination, false);
 
         match going_to {
             Going::Straight => Self {
                 points: vec![start_point, end_point],
             },
-            Going::Left => Self {
-                points: vec![start_point, end_point],
-            },
-            Going::Right => Self {
-                points: vec![start_point, end_point],
-            },
+            Going::Left | Going::Right => {
+                let start_strength_length = match going_to {
+                    Going::Left => STRAIGHT_LENGTH + ROAD_WIDTH / 2.0, // TODO: maybe change
+                    _ => STRAIGHT_LENGTH,
+                };
+
+                let curve_start_point =
+                    straight_point(coming_from, start_point, start_strength_length);
+
+                let curve_end_point = straight_point(destination, end_point, STRAIGHT_LENGTH);
+
+                Self {
+                    points: vec![start_point, curve_start_point, curve_end_point, end_point],
+                }
+            }
         }
     }
 
