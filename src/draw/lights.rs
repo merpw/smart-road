c3ac5cbd169a10c195b732b5778e654e4cc46d8a
Maksim Mikhailov <max@mer.pw>
2023-09-10 22:34:37 +0300

    Fix lights draw position

--- src/draw/lights.rs
+++ src/draw/lights.rs
@@ -5,10 +5,12 @@ use std::ops::Sub;
 
 pub fn draw_light(line: &Line) {
     let pos = match line.coming_from {
-        Direction::North => BOTTOM_LEFT.sub(Vec2::new(LIGHTS_SIZE, 0.0)),
-        Direction::East => TOP_LEFT.sub(Vec2::new(LIGHTS_SIZE, LIGHTS_SIZE)),
-        Direction::South => TOP_RIGHT.sub(Vec2::new(0.0, LIGHTS_SIZE)),
-        Direction::West => BOTTOM_RIGHT,
+        Direction::North => TOP_LEFT.sub(Vec2::new(LIGHTS_SIZE, LIGHTS_SIZE)),
+
+        Direction::East => TOP_RIGHT.sub(Vec2::new(0.0, LIGHTS_SIZE)),
+        Direction::South => BOTTOM_RIGHT,
+
+        Direction::West => BOTTOM_LEFT.sub(Vec2::new(LIGHTS_SIZE, 0.0)),
     };
 
     draw_rectangle(
