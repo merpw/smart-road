4d3728cdf0d74452b43e0d7090fd68112fe72408
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-11-05 17:01:42 +0200

    remove unused params, rename window title from Road Intersection to smart-road

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,7 @@
-use macroquad::color::Color;
 use macroquad::window::Conf;
 
 pub const WINDOW_SIZE: i32 = 1000;
 pub const ROAD_WIDTH: f32 = 500.0;
-
-pub const ROAD_LINE_WIDTH: f32 = 5.0;
-
-pub const ROAD_COLOR: Color = Color::new(0.267, 0.294, 0.325, 1.0);
-pub const ROAD_LINE_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.0);
 
 pub const CAR_WIDTH: f32 = 30.0;
 pub const CAR_LENGTH: f32 = 50.0;
@@ -20,7 +14,7 @@ pub const MAX_CAR_SPEED: f32 = 3.0;
 
 pub fn window_conf() -> Conf {
     Conf {
-        window_title: "Road Intersection | Grit:lab".to_owned(),
+        window_title: "smart-road | Grit:lab".to_owned(),
         window_width: WINDOW_SIZE,
         window_height: WINDOW_SIZE,
         window_resizable: false,
@@ -31,16 +25,3 @@ pub fn window_conf() -> Conf {
 // Helper constants
 
 pub const STRAIGHT_LENGTH: f32 = (WINDOW_SIZE as f32 - ROAD_WIDTH) / 2.0;
-/*
-// center square corners
-pub const TOP_LEFT: Vec2 = Vec2::new(STRAIGHT_LENGTH, STRAIGHT_LENGTH);
-
-pub const TOP_RIGHT: Vec2 = Vec2::new(WINDOW_SIZE as f32 - STRAIGHT_LENGTH, STRAIGHT_LENGTH);
-
-pub const BOTTOM_LEFT: Vec2 = Vec2::new(STRAIGHT_LENGTH, WINDOW_SIZE as f32 - STRAIGHT_LENGTH);
-
-pub const BOTTOM_RIGHT: Vec2 = Vec2::new(
-    WINDOW_SIZE as f32 - STRAIGHT_LENGTH,
-    WINDOW_SIZE as f32 - STRAIGHT_LENGTH,
-);
-*/
