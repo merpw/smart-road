2de46e9327d08a2e83c25191ec615003887bc72a
Maksim Mikhailov <max@mer.pw>
2023-11-14 13:58:57 +0200

    Improve algorithm
    Check for collisions only intersected paths

--- src/traffic/path_collisions.rs
+++ src/traffic/path_collisions.rs
@@ -1,11 +1,11 @@
 use crate::traffic::car::CarStatus::Turning;
-use crate::traffic::{Car, Direction, Going, Line, Path, TrafficState};
+use crate::traffic::{Direction, Going, Path, TrafficState};
 use std::rc::Rc;
 
 impl Path {
-    /// Returns all paths that collide with this path and has a car in the intersection
+    /// Returns all paths that collide with this path and have a car that is turning (possible collision)
     pub fn get_used_collision_paths(&self, traffic_state: &TrafficState) -> Vec<Rc<Path>> {
-        self.get_collision_paths(traffic_state)
+        self.get_intersected_paths(traffic_state)
             .iter()
             .filter(|path| {
                 let cars = traffic_state.lines[path.coming_from as usize].path_cars(path);
@@ -16,29 +16,85 @@ impl Path {
     }
 
     /// Returns all paths that collide with this path
-    fn get_collision_paths(&self, traffic_state: &TrafficState) -> Vec<Rc<Path>> {
-        let all_paths = traffic_state
+    fn get_intersected_paths(&self, traffic_state: &TrafficState) -> Vec<Rc<Path>> {
+        let path_definitions: Vec<(Direction, Going)> = match (self.coming_from, self.going_to) {
+            (_, Going::Right) => vec![],
+            (Direction::South, Going::Left) => {
+                vec![
+                    (Direction::West, Going::Straight),
+                    (Direction::East, Going::Left),
+                    (Direction::West, Going::Left),
+                    (Direction::North, Going::Straight),
+                ]
+            }
+            (Direction::South, Going::Straight) => {
+                vec![
+                    (Direction::West, Going::Straight),
+                    (Direction::North, Going::Left),
+                    (Direction::East, Going::Left),
+                    (Direction::East, Going::Straight),
+                ]
+            }
+            (Direction::North, Going::Left) => {
+                vec![
+                    (Direction::East, Going::Straight),
+                    (Direction::West, Going::Left),
+                    (Direction::East, Going::Left),
+                    (Direction::South, Going::Straight),
+                ]
+            }
+            (Direction::North, Going::Straight) => {
+                vec![
+                    (Direction::East, Going::Straight),
+                    (Direction::South, Going::Left),
+                    (Direction::West, Going::Left),
+                    (Direction::West, Going::Straight),
+                ]
+            }
+            (Direction::East, Going::Left) => {
+                vec![
+                    (Direction::South, Going::Straight),
+                    (Direction::North, Going::Left),
+                    (Direction::South, Going::Left),
+                    (Direction::West, Going::Straight),
+                ]
+            }
+            (Direction::East, Going::Straight) => {
+                vec![
+                    (Direction::South, Going::Straight),
+                    (Direction::West, Going::Left),
+                    (Direction::North, Going::Left),
+                    (Direction::North, Going::Straight),
+                ]
+            }
+            (Direction::West, Going::Left) => {
+                vec![
+                    (Direction::North, Going::Straight),
+                    (Direction::South, Going::Left),
+                    (Direction::North, Going::Left),
+                    (Direction::East, Going::Straight),
+                ]
+            }
+            (Direction::West, Going::Straight) => {
+                vec![
+                    (Direction::North, Going::Straight),
+                    (Direction::East, Going::Left),
+                    (Direction::South, Going::Left),
+                    (Direction::South, Going::Straight),
+                ]
+            }
+        };
+
+        traffic_state
             .lines
             .iter()
             .flat_map(|line| line.paths.iter())
             .filter(|path| {
-                if path.going_to == Going::Right {
-                    return false;
-                }
-
-                if path.coming_from == self.coming_from {
-                    // same line
-                    return false;
-                }
-
-                true
+                path_definitions.iter().any(|&(coming_from, going_to)| {
+                    path.coming_from == coming_from && path.going_to == going_to
+                })
             })
             .cloned()
-            .collect::<Vec<_>>();
-
-        match (self.coming_from, self.going_to) {
-            (_, Going::Right) => vec![],
-            _ => all_paths,
-        }
+            .collect()
     }
 }
